7d210593c03a7f999724d0e2ca63276770fc4648
Vincenzo Acinapura <vin.aci@gmail.com>
2012-06-15 18:07:48 +0200

    Simplifyed version of config.rb file

--- config.rb
+++ config.rb
@@ -2,67 +2,15 @@ Encoding.default_external = 'UTF-8'
 
 require 'coffee-filter'
 
+activate :i18n, :mount_at_root => :en
+
 set :relative_links, true
+set :build_dir, 'public'
 
-###
-# Compass
-###
+["*slides*", "*form*"].each do |page|
+  page "#{page}", :layout => false
+end
 
-# Susy grids in Compass
-# First: gem install compass-susy-plugin
-# require 'susy'
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-page "form.php", :layout => false
-page "it/form.php", :layout => false
-
-page "form-booking-success.html", :layout => false
-page "it/form-booking-success.html", :layout => false
-
-page "form-booking-warning.php", :layout => false
-page "it/form-booking-warning.php", :layout => false
-
-page "slides.html", :layout => false
-page "it/slides.html", :layout => false
-
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy (fake) files
-# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
-#   @which_fake_page = "Rendering a fake page with a variable"
-# end
-
-# ['index', 'villas', 'activities', 'credits', 'privacy'].each do |page|
-#   page "#{page}.html", :proxy => "/localizable/standard.html" do
-#     @page = page
-#   end
-# end
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Methods defined in the helpers block are available in templates
 helpers do
   def localized_path(path, extension = 'html')
     path = I18n.t "paths.#{path}"
@@ -70,30 +18,8 @@ helpers do
   end
 end
 
-# set :css_dir, 'stylesheets'
-# set :js_dir, 'javascripts'
-# set :images_dir, 'images'
-
-activate :i18n, :mount_at_root => :en
-
-set :build_dir, 'public'
-
-# Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
-  activate :minify_css
-
-  # Minify Javascript on build
-  activate :minify_javascript
-
-  # Enable cache buster
-  # activate :cache_buster
-
-  # Use relative URLs
   activate :relative_assets
-
-  # Compress PNGs after build
-  # First: gem install middleman-smusher
-  # require "middleman-smusher"
-  # activate :smusher
+  # activate :minify_css
+  # activate :minify_javascript
 end
